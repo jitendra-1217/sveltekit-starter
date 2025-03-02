27bf6a0d1f8fe965e19829a29c092e2b13afcc85
jitendra-1217 <jitendraojha1217@gmail.com>
2025-03-03 00:06:08 +0530

    Adds fetch hooks

---
+++ src/hooks.client.ts
@@ -0,0 +1,20 @@
+import { ulid } from 'ulid';
+
+const originalFetch = window.fetch;
+window.fetch = function (input: RequestInfo | URL, init?: RequestInit): Promise<Response> {
+  const requestId = ulid();
+  const enhancedInit: RequestInit = { ...init };
+
+  if (!enhancedInit.headers) {
+    enhancedInit.headers = {};
+  }
+
+  const headers = enhancedInit.headers instanceof Headers
+    ? enhancedInit.headers
+    : new Headers(enhancedInit.headers as HeadersInit);
+
+  headers.set('X-Request-Id', requestId);
+  enhancedInit.headers = headers;
+
+  return originalFetch(input, enhancedInit);
+};
---
+++ src/hooks.server.ts
@@ -0,0 +1,26 @@
+import { ulid } from 'ulid';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const originalFetch = event.fetch;
+
+  event.fetch = function (input: RequestInfo | URL, init?: RequestInit): Promise<Response> {
+    const requestId = ulid();
+    const enhancedInit: RequestInit = { ...init };
+
+    if (!enhancedInit.headers) {
+      enhancedInit.headers = {};
+    }
+
+    const headers = enhancedInit.headers instanceof Headers
+      ? enhancedInit.headers
+      : new Headers(enhancedInit.headers as HeadersInit);
+
+    headers.set('X-Request-Id', requestId);
+    enhancedInit.headers = headers;
+
+    return originalFetch(input, enhancedInit);
+  };
+
+  return resolve(event);
+};
